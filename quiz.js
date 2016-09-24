ce0cefe3b77a166afca9aee6fb78c49960bb30b3
SmithingChick <sharon.smith.metals@gmail.com>
2016-09-23 17:55:35 -0700

    initial file upload

---
+++ quiz.js
@@ -0,0 +1,7 @@
+var answer = prompt('What programing language is the name of a gem?');
+if (  answer.toUpperCase === 'RUBY' ) {
+  document.write("<p>That's right!</p>");
+} else {
+  document.write("<p>Sorry, that's wrong.</p>");
+}
+
